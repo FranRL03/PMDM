24b30d4aca3bd42e732917defb4ea170012cbae2
Fran Ruiz <fjavierrule@gmail.com>
2023-09-21 19:14:49 +0200

    codigo cesar

---
+++ Ejercicio cifrado Cesar/cifrado.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+
+
+    $(document).on('click', '#boton', () => {
+
+        var abecedario = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+        var tres = 3;
+        var text = $('#mensaje').val();
+        var valores = [];
+        
+
+        for (var i = 0; i < text.length; i++) {
+
+            var valor = text.charAt(i).charCodeAt();
+
+            var valorNuevo = (valor - 97 + tres) % 26 + 1;
+
+            valores.push(valorNuevo);
+
+        }
+
+        console.log(valores)
+        console.log(valorNuevo)
+
+        for(var i = 0; abecedario.length; i++){
+
+            var numeros = valores[i];
+
+            var letra = abecedario[numeros];
+
+        }
+
+       
+        console.log(letra)
+        
+
+    });
+});
+
