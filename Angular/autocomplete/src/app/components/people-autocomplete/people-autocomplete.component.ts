64524198601168afd6028eb3a77a437cd882e78a
Fran Ruiz <fjavierrule@gmail.com>
2023-10-29 12:06:48 +0100

    autocomplete

--- Angular/autocomplete/src/app/components/people-autocomplete/people-autocomplete.component.ts
+++ Angular/autocomplete/src/app/components/people-autocomplete/people-autocomplete.component.ts
@@ -1,20 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Observable, map, startWith } from 'rxjs';
+import { Observable, debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs';
 import { People } from 'src/app/model/people-list.interface';
+import { PeopleListService } from 'src/app/services/people.sevice';
 
 @Component({
   selector: 'app-people-autocomplete',
   templateUrl: './people-autocomplete.component.html',
   styleUrls: ['./people-autocomplete.component.css']
 })
-export class PeopleAutocompleteComponent {
+export class PeopleAutocompleteComponent{
 
 
-  peopleCtrl = new FormControl('');
+  peopleCtrl = new FormControl();
   filterPeople: Observable<People[]>;
   people : People[] = [];
-
+  
 
   constructor() {
     this.filterPeople = this.peopleCtrl.valueChanges.pipe(
