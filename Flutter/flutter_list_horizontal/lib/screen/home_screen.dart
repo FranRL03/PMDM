494d38cd02a86a72d36061306ab41f3a2d31ecc7
Fran Ruiz <fjavierrule@gmail.com>
2024-01-11 18:19:36 +0100

    terminar el diseño

--- Flutter/flutter_list_horizontal/lib/screen/home_screen.dart
+++ Flutter/flutter_list_horizontal/lib/screen/home_screen.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_list_horizontal/widget/ActorsList.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      body: Padding(
+      padding: const EdgeInsets.only(left: 40, top: 80),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+        const Text('Best actors',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),),
+        const Text('and actresses', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30)),
+        const Text('March 2024', style: TextStyle(color: Colors.blueGrey)),
+
+         Expanded(
+           child: ListView(
+             scrollDirection: Axis.horizontal,
+            children: const [
+              ActorsList(name: 'Morgan', apellido: 'Freeman', url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/jPsLqiYGSofU4s6BjrxnefMfabb.jpg'),
+              ActorsList(name: 'Morgan', apellido: 'Freeman', url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/jPsLqiYGSofU4s6BjrxnefMfabb.jpg'),
+              ActorsList(name: 'Morgan', apellido: 'Freeman', url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/jPsLqiYGSofU4s6BjrxnefMfabb.jpg'),
+              ActorsList(name: 'Morgan', apellido: 'Freeman', url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/jPsLqiYGSofU4s6BjrxnefMfabb.jpg'),
+            ]),
+         ),
+      ]),   
+      ),
+     
+
+      //  ListView(
+      //     children: const [
+      //       ActorList(name: 'Morgan', apellido: 'Freeman', url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/jPsLqiYGSofU4s6BjrxnefMfabb.jpg'),
+      //       ActorList(name: 'Morgan', apellido: 'Freeman', url: 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2/jPsLqiYGSofU4s6BjrxnefMfabb.jpg')
+      //     ]),
+      );
   }
 }
--- Flutter/flutter_list_horizontal/lib/widget/ActorsList.dart
+++ Flutter/flutter_list_horizontal/lib/widget/ActorsList.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
-class ActorList extends StatelessWidget {
-  const ActorList({super.key});
+class ActorsList extends StatelessWidget {
+  final String name, apellido, url;
+
+  const ActorsList(
+      {super.key,
+      required this.name,
+      required this.apellido,
+      required this.url});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        scrollDirection: Axis.horizontal,
-        children: [],
+    return SizedBox(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 40, right: 20),
+        child: Column(children: [
+          Container(
+              width: 150,
+              height: 350,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: const BorderRadius.vertical(
+                  top: Radius.circular(800), // Controla la curvatura superior
+                  bottom: Radius.circular(800), // Controla la curvatura inferior
+                ),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: NetworkImage(url),
+                ),
+              )),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+            Text(name),
+            Text(apellido),
+          ]),
+        ]),
       ),
     );
   }
