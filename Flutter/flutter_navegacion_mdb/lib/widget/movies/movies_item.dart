82baa0e2f5c1184e5e44197fa374f47786051800
Fran Ruiz <fjavierrule@gmail.com>
2024-01-25 12:35:31 +0100

    implementacion del circulo de carga

--- Flutter/flutter_navegacion_mdb/lib/widget/movies/movies_item.dart
+++ Flutter/flutter_navegacion_mdb/lib/widget/movies/movies_item.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_navegacion_mdb/widget/movies/movie_details.dart';
 
 import '../../models/movie_response/result.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class MovieItem extends StatelessWidget {
   const MovieItem({super.key, required this.result});
@@ -13,42 +14,58 @@ class MovieItem extends StatelessWidget {
     return Card(
       color: const Color.fromARGB(255, 230, 218, 218),
       margin: const EdgeInsets.all(10),
-      child: Column(
-        children: [
-          Image(
-            image: NetworkImage(
-              'https://media.themoviedb.org/t/p/w500${result.posterPath}',
-            ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: Stack(children: [
+          const Center(
+            child: CircularProgressIndicator(),
           ),
-          Row(
+          Column(
             children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text(
-                    '${result.title}',
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.w600),
-                  ),
-                ),
+              // Image(
+              //   image: NetworkImage(
+              //     'https://media.themoviedb.org/t/p/w500${result.posterPath}',
+              //   ),
+              // ),
+              FadeInImage.memoryNetwork(
+                placeholder: kTransparentImage,
+                image:
+                    'https://media.themoviedb.org/t/p/w500${result.posterPath}',
+                // width: 150,
+                // height: 300,
+                fit: BoxFit.cover,
               ),
-              Padding(
-                padding: const EdgeInsets.only(right: 20, bottom: 10),
-                child: ElevatedButton(
-                  onPressed: () {
-                     Navigator.push(
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20),
+                      child: Text(
+                        '${result.title}',
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20, bottom: 10),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
                             context,
                             MaterialPageRoute(
                                 builder: (context) => MovieDetailsPage(
                                       movieDetails: result,
                                     )));
-                  },
-                  child: const Text('Info'),
-                ),
+                      },
+                      child: const Text('Info'),
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ]),
       ),
     );
   }
