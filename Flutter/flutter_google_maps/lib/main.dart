8f009c05f1cd53e203cc7cd497739421f0cdd2d6
Fran Ruiz <fjavierrule@gmail.com>
2024-01-28 20:06:47 +0100

    google maps

--- Flutter/flutter_google_maps/lib/main.dart
+++ Flutter/flutter_google_maps/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_google_maps/pages/map_page.dart';
+import 'package:flutter_google_maps/pages/map_widget.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,6 +10,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: MapPage());
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MapWidget(),
+    );
   }
 }
